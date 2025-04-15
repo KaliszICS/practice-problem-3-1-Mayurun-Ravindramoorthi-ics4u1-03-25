25c0dba0109be738fa90359e02b8942c1e8199e0
Mayurun-Ravindramoorthi-ics4u1-03-25 <mayurun.ravindramoorthi@student.tdsb.on.ca>
2025-04-15 02:14:47 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,26 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(factorial(5));
 	}
 
-	
+	public static int sumOfDigits(int x) {
+		if(x<0){
+			x= x*-1;
+		}
+		if (x == 0)
+			return 0;
+		return (x % 10) + sumOfDigits(x / 10);
+	}	
+	public static int factorial(int n) {
+		if (n<0){
+			return 0;
+		}
+		if (n==0){
+			return 1;
 
+		}
+		return n*factorial(n-1);
 }
+}
+
